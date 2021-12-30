720f2c2106aaae52531cfd25ab08f8a2f07bd743
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-30 08:52:04 -0700

    Create Customer

---
+++ src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,77 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "customer_name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "notes")
+    private String notes;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Pet> pets;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String phoneNumber, String notes) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long customerID) {
+        this.id = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+}
