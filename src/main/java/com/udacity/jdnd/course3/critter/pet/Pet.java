35f31941f30f3a72a69670369f650be288f396c2
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-30 08:51:10 -0700

    Create Pet

---
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,103 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.schedule.Schedule;
+import com.udacity.jdnd.course3.critter.user.Customer;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pet")
+public class Pet {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PetType type;
+
+    @Column(name = "pet_name")
+    private String name;
+
+    @Column(name = "birthDate")
+    private LocalDate birthDate;
+
+    @Column(name = "notes")
+    private String notes;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    public Pet() {
+    }
+
+    public Pet(PetType type, String name, Customer owner, LocalDate birthDate, String notes) {
+        this.type = type;
+        this.name = name;
+        this.customer = owner;
+        this.birthDate = birthDate;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long petID) {
+        this.id = petID;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    public void setCustomer(Customer owner) {
+        this.customer = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+}
