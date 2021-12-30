078afaf10c8fa747c5c88493e531a5165f1f6231
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-30 08:51:44 -0700

    Create Schedule

---
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
@@ -0,0 +1,82 @@
+package com.udacity.jdnd.course3.critter.schedule;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "schedule_id")
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "schedule", cascade = CascadeType.ALL)
+    private List<Employee> employees;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "schedule", cascade = CascadeType.ALL)
+    private List<Pet> pets;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "EmployeeSkill")
+    @JoinColumn(name = "id")
+    private Set<EmployeeSkill> activities;
+
+    public Schedule() {
+    }
+
+    public Schedule(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
